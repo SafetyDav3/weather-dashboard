b64f464fe3f11a10bfac839b18b286fdf65f9b0b
SafteyDav3 <talktomedw@gmail.com>
2022-04-10 01:26:15 -0500

    Assest

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -14,7 +14,25 @@ cityList.appendChild(cityEl);
 //
 
 // ↑←↓ Fetch geo data fromm Geocoding API  ↓→↑
+cityName = "Greenbay";
+apiKey = "8e937896277dbf3b8a02a76dc6fc7336";
+stateCode = "wi";
+countryCode = "840";
 
+var api =
+  "http://api.openweathermap.org/geo/1.0/direct?q=" +
+  cityName +
+  "&" +
+  countryCode +
+  "&appid=" +
+  apiKey;
+
+
+
+
+
+
+  console.log(api)
 // ↑←↓ Get current data  ↓→↑
 // ↑←↓ Attach current data to li's in "conditions-list"  ↓→↑
 
